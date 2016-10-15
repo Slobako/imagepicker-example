c54d7f60fb5275721d6e13a5ba2471a879032ac6
Slobako <kovrlija.slobodan@gmail.com>
2016-10-14 23:20:26 -0400

    project created

---
+++ XYImagePicker/XYImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYImagePicker
+//
+//  Created by Slobodan Kovrlija on 10/14/16.
+//  Copyright © 2016 Slobodan Kovrlija. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
